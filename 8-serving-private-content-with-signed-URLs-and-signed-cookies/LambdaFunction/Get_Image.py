cb4173605c6211db0896ecf33dd411496f7c3785
awsdemo kt <awsdemo.kt@gmail.com>
2020-08-16 17:48:19 +1000

    made changes to Get_Image

--- 8-serving-private-content-with-signed-URLs-and-signed-cookies/LambdaFunction/Get_Image.py
+++ 8-serving-private-content-with-signed-URLs-and-signed-cookies/LambdaFunction/Get_Image.py
@@ -9,19 +9,18 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from botocore.signers import CloudFrontSigner
 
-
 def get_secret():
-    # make sure you have a CloudFront Private Key stored in Secrets Manager with with name "cloudfrontkey"
     secret_name = "cloudfrontkey"
     region_name = "ap-southeast-2"
 
-    # Create a Secrets Manager client
+    
     session = boto3.session.Session()
+    # Reads a cloudfront private key from the secret manager, you need to create a secret manually with the name "cloudfrontkey"
+    # storing cloudfront private key
     client = session.client(
         service_name='secretsmanager',
         region_name=region_name
     )
-
     try:
         get_secret_value_response = client.get_secret_value(
             SecretId=secret_name
@@ -30,6 +29,8 @@ def get_secret():
     except ClientError as e:
         raise e
     else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
         if 'SecretString' in get_secret_value_response:
             secret = get_secret_value_response['SecretString']
         else:
@@ -45,27 +46,31 @@ def rsa_signer(message):
 
 def getCloudFrontDistributionName():
     client = boto3.client('ssm')
+    #cf-distribution-name parameter created in Systems Manager Parameter Store by Cloudformation stack
     cf_distrubution = client.get_parameter(
         Name='cf-distribution-name',
         WithDecryption=False
     )
     return cf_distrubution['Parameter']['Value']
 
+def getCloudFrontKeyId():
+    client = boto3.client('ssm')
+    #you need to create a parameter manually in the parameter store with the name CloudFrontAccessKeyID, storing Cloudfront accesskey id
+    cloudfront_access_keyid = client.get_parameter(
+        Name='CloudFrontAccessKeyID',
+        WithDecryption=False
+    )
+    return cloudfront_access_keyid['Parameter']['Value']
+
 def getImagesBucketName():
     client = boto3.client('ssm')
+    #images-bucket parameter created in Systems Manager Parameter Store by Cloudformation stack
     images_bucket = client.get_parameter(
         Name='images-bucket',
         WithDecryption=False
     )
     return images_bucket['Parameter']['Value']
     
-def getCloudFrontAccessKeyId():
-    client = boto3.client('ssm')
-    cloudfrontAccessKeyId = client.get_parameter(
-        Name='CloudFrontAccessKeyID',
-        WithDecryption=False
-    )
-    return cloudfrontAccessKeyId['Parameter']['Value']
 
 def get_image(event, context):
     event_string = json.dumps(event)
@@ -78,17 +83,22 @@ def get_image(event, context):
             "body": json.dumps({
                 "message": "You are trying to access premium property images, you need to subscribe for premium membership.",
             }),
-            "headers":{ 'Access-Control-Allow-Origin' : '*' }
+              "headers":{ 'Access-Control-Allow-Origin' : '*' }
         }
+
     bucket_name = getImagesBucketName()
     print("Bucket name is {0}".format(bucket_name))
     
     try:
         #key_id = 'xxxxxxxxxxxxxxxxxxx'
-        key_id = getCloudFrontAccessKeyId()
-        print("Cloudfront Access Key Id is {0}".format(key_id))
+        # reading value from parameter store
+        key_id = getCloudFrontKeyId()
+        print("Cloudfront Access key id is {0}".format(key_id))
+
+        # reading value from parameter store    
         cf_distribution_name = getCloudFrontDistributionName()
         print("Cloudfront distribution name is {0}".format(cf_distribution_name))
+
         url = "https://" + cf_distribution_name + "/" + image_name
         expire_date = datetime.datetime(2020, 10, 10)
         cloudfront_signer = CloudFrontSigner(key_id, rsa_signer)
@@ -96,8 +106,8 @@ def get_image(event, context):
         # Create a signed url that will be valid until the specfic expiry date
         # provided using a canned policy.
         signed_url = cloudfront_signer.generate_presigned_url(url, date_less_than=expire_date)
-        print(signed_url)
-        print("looking for image name is {0} from bucket {1}".format(image_name,bucket_name))
+        print("Pre-signed URL is {0}".format(signed_url))
+                
         s3 = boto3.resource('s3')
         object = s3.Object(bucket_name,image_name).load()
         msg = "Image with name : " + image_name + " exists"
--- 8-serving-private-content-with-signed-URLs-and-signed-cookies/README.md
+++ 8-serving-private-content-with-signed-URLs-and-signed-cookies/README.md
@@ -10,7 +10,8 @@ This is what we will build here
 - You need to have a root access to AWS account to create CloudFront key to generate pre-signed url.
 - Generate a CloudFront key, download the private key and store it in AWS Secret manager
   - secret name must be **cloudfrontkey**
-- Take note of CloudFront key id and secret name that you use to store CloudFront private key in the secret manager.
+- Store CloudFront Access Key Id in the parameter store (Systems Managers), 
+Take note of CloudFront Access Key Id and secret name that you use to store CloudFront private key in the secret manager.
 - Create an EC2 instance, select Amazon Linux 2
 - Assign it an admin role as we will deploy this from EC2 due to a constraint with cryptography library that we are using to generate pre-signed url
 - Login into EC2 instance and install git
